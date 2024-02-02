d1e4bb6d13e9344cf0e28872a749d5bf30c65362
bunda3d <bunda3@yahoo.com>
2024-02-02 00:50:30 -0600

    update blogpost list ui, component, service

--- src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from '../services/blog-post.service';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
   styleUrls: ['./blogpost-list.component.css']
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
 
+  //create observable var to subscribe to
+  blogPosts$?: Observable<BlogPost[]>;
+  
+  constructor(private blogPostService: BlogPostService) {
+    //constructor to inject the service
+  }
+
+  ngOnInit(): void { 
+    //get all blogs from api using async pipe subscription to handle sub/unsub since it's readonly (not binding to form inputs)
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
 }
